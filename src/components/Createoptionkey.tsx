25777d3f2820a539528019a02177a23b84f6a650
Chinthu Damodhar Reddy <102827663+chinthudamodharreddy-CH-2022@users.noreply.github.com>
2023-10-16 19:43:43 +0530

    made allignment modifications for option key

--- src/components/Createoptionkey.tsx
+++ src/components/Createoptionkey.tsx
@@ -1,16 +1,15 @@
-import * as React from 'react';
-import '../App.css';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Popover from '@mui/material/Popover';
-import TextField from '@mui/material/TextField';
-import { useNavigate } from 'react-router-dom';
-import tick from '../../src/assests/tickimage.png';
+import * as React from "react";
+import "../App.css";
+import Popover from "@mui/material/Popover";
+import { useNavigate } from "react-router-dom";
+import {useForm} from 'react-hook-form'
 
 export default function ValidationTextFields() {
   const navigate = useNavigate();
 
-  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
+    null
+  );
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
@@ -22,63 +21,38 @@ export default function ValidationTextFields() {
 
   const handleOkClick = () => {
     navigate("/");
-  }
+  };
 
   const open = Boolean(anchorEl);
-  const id = open ? 'simple-popover' : undefined;
-  
-  return (
-    <>
-      <text  className="title-optionkey">Create OptionKey</text>
-    <Box
-    className="box-createoptionkey"
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-     
+  const id = open ? "simple-popover" : undefined;
+  const form = useForm();
 
-      <div>
-        <TextField
-          error
-          id="outlined-error"
-          label="Name"
-          defaultValue=""
-        />
-        <TextField
-          error
-          id="outlined-error"
-          label="Image"
-          defaultValue=""
-          helperText="Incorrect entry."
-        />
-      </div>  
-     
-        <Popover
-          id={id}
-          open={open}
-          anchorEl={anchorEl}
-          onClose={handleClose}
-          // anchorOrigin={{
-          //   vertical: 'bottom',
-          //   horizontal: 'left',
-          // }}
-          className=""
-        >
-          <div className="popup-ok">
-            <img src={tick} className='tickimg' alt="tick" />
-            <label className="text">Option Key Created successfully </label>
-            <button onClick={handleOkClick} className="button-ok" >OK</button>
+  return (
+    <div>
+      <text className="title-optionkey">Create OptionKey</text>
+
+      <form className="form-optionkey">
+
+        <div className="row1-optionkey">
+          <div className="name-div">
+            <label htmlFor="name">Name</label>
+            <input className="input-name" type="text" id="name" name="name" />
           </div>
-        </Popover>
-     
-    </Box>
-    <div className="button-createoptionkey">
-      <Button onClick={handleClick}  variant="contained">Submit</Button>
+
+          <div className="image-div">
+            <label htmlFor="name">Image</label>
+            <input
+              className="input-image"
+              type="text"
+              id="image"
+              name="image"
+            />
+          </div>
+        </div>
+
+        <button className="button-optionkey">Submit</button>
+      
+      </form>
     </div>
-    </>
   );
 }
